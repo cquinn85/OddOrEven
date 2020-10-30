5168ee917bc497b1f034cea60322297afb3cbb6d
cquinn85 <cequinn300@gmail.com>
2020-10-30 10:21:53 -0500

    Method practice complete

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,26 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Pick a number:");
+            IsOdd();
+        }
+
+        public static void IsOdd()
+        {
+            var num = int.Parse(Console.ReadLine());
+            var rem = num % 2;
+
+            if(rem == 0)
+            {
+                Console.WriteLine("Your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd.");
+            }
+           
         }
     }
+
 }
